d0fd7f2ef2bd29836f6de8cac6b1129d11405f2b
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-05-07 16:23:12 +0300

    new solution

--- script.js
+++ script.js
@@ -7,25 +7,55 @@ const sumOfArguments = (...args) => {
 
 console.log(sumOfArguments(1, 2, 3));
 
+function sum() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
+console.log(sum(1, 2, 3, 10));
+
+
 
 // 8 
 // Write a function which removes items from the middle of an array and replace with three items.
 const cities = ['kathmandu', 'pokhara', 'chitwan', 'Dolakha', 'Salyan', 'Mustang']
 
-const removeMiddleItem = (arr, ...args) => {
+const removeMiddleItem = (arr, city1, city2, city3) => {
 
     let middleItem = Math.floor(arr.length / 2);
 
     if (arr.length % 2 === 0) {
+        arr.splice((middleItem - 1), 2, city1, city2, city3);
 
-        arr.splice((middleItem - 1), 2, args);
+    } else if (arr.length % 2 !== 0) {
+        arr.splice(middleItem, 1, city1, city2, city3);
+
+    } else {
+        console.log('Error...')
     }
-
-    if (arr.length % 2 !== 0) {
-
-        arr.splice(middleItem, 1, args);
-    }
-    return arr
+    return arr;
 }
 
-console.log(removeMiddleItem(cities, 'Helsinki', 'Rovaniemi'));
+console.log(removeMiddleItem(cities, 'Helsinki', 'Espoo', 'Vantaa'));
+
+// 10 
+// Create a function that takes two strings and returns true if the first argument ends with the second argument; 
+// otherewise return false . 
+// Take two strings as arguments. 
+// Determine if second string matches ending of first string. Return boolean value. Example
+
+const checkEnding = (arg1, arg2) => {
+
+    const pattern = new RegExp(arg2);
+
+    if (arg1.match(pattern)) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(checkEnding('Hari', 'vri'));
