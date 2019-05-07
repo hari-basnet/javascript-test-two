4ca7d3d24f47b6301e97d8566ca1799aa17dc8d0
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-05-07 10:34:21 +0300

    new challenges added

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 // 7
 //Write a function which takes any number of arguments and return the sum of the arguments
 
-function sumOfArguments (...args) {
-
-     return args.reduce((amount, items) => amount += items);
+const sumOfArguments = (...args) => {
+    return args.reduce((amount, item) => amount += item);
 }
 
-console.log(sumOfArguments(1,2,3));
+console.log(sumOfArguments(1, 2, 3));
 
 
 // 8 
 // Write a function which removes items from the middle of an array and replace with three items.
+const cities = ['kathmandu', 'pokhara', 'chitwan', 'Dolakha', 'Salyan', 'Mustang']
 
-function removeMiddleItem (arr) {
+const removeMiddleItem = (arr, ...args) => {
 
-    let middleOfOdd = Math.floor(arr.length / 2); 
+    let middleItem = Math.floor(arr.length / 2);
+
+    if (arr.length % 2 === 0) {
+
+        arr.splice((middleItem - 1), 2, args);
+    }
+
+    if (arr.length % 2 !== 0) {
+
+        arr.splice(middleItem, 1, args);
+    }
+    return arr
 }
+
+console.log(removeMiddleItem(cities, 'Helsinki', 'Rovaniemi'));
